68c287d60581497c131d44029287c887d4c3589d
jonasjesus42 <jonasdasilvajesus@outlook.com>
2022-02-25 10:06:28 -0300

    criando as rotas e as paginas

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,18 @@
-
+import {Routes, Route} from "react-router-dom"
+import Home from "./pages/home"
+import Destino from "./pages/destino"
+import Promocoes from "./pages/promocoes"
+import Contato from "./pages/contato"
 
 function App() {
 
   return (
-    <h1>home</h1>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/destino" element={<Destino />} />
+      <Route path="/promocoes" element={<Promocoes />} />
+      <Route path="/contato" element={<Contato />} />
+    </Routes>
   )
 }
 
---
+++ front/src/pages/promocoes.jsx
@@ -0,0 +1,7 @@
+export default function Promocoes(){
+    return(
+        <>
+            <h1>Promoções</h1>
+        </>
+    )
+}
